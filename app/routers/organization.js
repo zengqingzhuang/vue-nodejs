398d6ded3737b0915abd36dd261f4229e7bf08ca
Zeng,Qingzhuang <zengqingzhuang@baidu.com>
2017-04-20 09:47:32 +0800

    up

--- app/routers/organization.js
+++ app/routers/organization.js
@@ -4,16 +4,17 @@
  */
 let express = require('express');
 let router = express.Router();
-let Log = require('../tools/log').logger;
-let log4 = require('log4js').getLogger();
+let helpers = require('../helpers');
+let log = require('../tools/log');
 let orgModel = require('../models/organization');
 router.get('/organization/list.:format', (req, res, next) => {
 	//可以请求接口，如果报错直接走next(err);
-	Log('error', '接口:' + req.originalUrl);
-	log4.error('sssssssss')
-	orgModel.getOrgList({
-		id: 1
-	}, (err, result) => {
+	let condition = helpers.getRequestParams(req);
+	log.error('异步接口:' + req.originalUrl, '传入参数:' + JSON.stringify(condition), '错误信息:' + JSON.stringify({
+		errno: 101,
+		message: 'id不能为空'
+	}));
+	orgModel.getOrgList(condition, (err, result) => {
 		if (err) {
 			return next(err);
 		}
--- app/tools/log.js
+++ app/tools/log.js
@@ -3,48 +3,25 @@
  * @author dazhuang(zengqingzhuang$baidu.com)
  *
  */
-let _ = require('lodash');
-let config = require('../../config');
 let log4js = require('log4js');
-let util = require('util');
 
-exports.logger = function() {
-    if (!config.outlogs) {
-        return;
+module.exports = {
+    error: function(...params) {
+        this.writeLog('error', params);
+    },
+    info: function(...params) {
+        this.writeLog('info', params);
+    },
+    warn: function(...params) {
+        this.writeLog('warn', params);
+    },
+    trace: function(...params) {
+        this.writeLog('trace', params);
+    },
+    debug: function(...params) {
+        this.writeLog('debug', params);
+    },
+    writeLog: function(type, params) {
+        log4js.getLogger(type)[type](params.join('|'));
     }
-    let type = "cheese",
-        logs = "log里什么也没有",
-        type_num = 0;
-    if (arguments.length === 0) {
-        logs = "log里什么也没有";
-    }
-    if (arguments.length === 1) {
-        logs = arguments[0];
-    }
-    if (arguments.length > 1) {
-        logs = [], type_num = 1;
-        type = arguments[0];
-        _.forEach([].slice.call(arguments, type_num, arguments.length), function(p, n) {
-            if (_.isObject(p)) {
-                if (util.isError(p)) {
-                    logs.push(p.message || p.messages[0]);
-                } else {
-                    logs.push(JSON.stringify(p));
-                }
-
-                return;
-            }
-            if (_.isArray(p)) {
-                logs.push(p.join(","));
-                return;
-            }
-            logs.push(p);
-        })
-        logs = logs.join("|");
-    }
-    //TODO log需要完善
-    //log4js.addAppender(log4js.appenders.file('./logs/trace.log'), 'logger');
-    let logger = log4js.getLogger(type);
-    console.log(logs,'111')
-    logger.trace(logs);
-};
+}
